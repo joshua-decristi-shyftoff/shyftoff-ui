b3804854b00622d033efc471f72416f97d80e63a
Joshua DeCristi <joshua.decristi@Mac.lan>
2025-09-28 19:23:53 -0400

    chore: move globals.css styles directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 
-import "@/app/globals.css";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   subsets: ["latin"],
