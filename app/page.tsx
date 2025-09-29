03c882e05383dcc24bdd9d1c91bf1b23681f0c8a
Joshua DeCristi <joshua.decristi@Joshuas-MacBook-Pro.local>
2025-09-29 18:33:43 -0400

    chore: fix linting for new rules

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,14 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
           priority
+          height={38}
+          width={180}
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing
+            {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
@@ -29,23 +30,23 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={20}
               height={20}
+              width={20}
             />
             Deploy now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Read our docs
           </a>
@@ -55,45 +56,45 @@ export default function Home() {
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <Image
-            aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
+            aria-hidden
             height={16}
+            width={16}
           />
           Learn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <Image
-            aria-hidden
             src="/window.svg"
             alt="Window icon"
-            width={16}
+            aria-hidden
             height={16}
+            width={16}
           />
           Examples
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <Image
-            aria-hidden
             src="/globe.svg"
             alt="Globe icon"
-            width={16}
+            aria-hidden
             height={16}
+            width={16}
           />
           Go to nextjs.org →
         </a>
